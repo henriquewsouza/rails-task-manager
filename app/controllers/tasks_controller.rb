1a96dbd60277d6b02b9fd1d5726afc4f695b03d8
Henrique Waxman <henriquewsouza@gmail.com>
2023-05-23 13:04:49 -0300

    new, show, create, index

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,32 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+    @task.completed = false
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+
+end
